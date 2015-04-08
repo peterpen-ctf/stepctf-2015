e95c1a1a4be1b82d84eb5eca8fd46d3d67431529
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2015-04-08 14:14:52 +0300

    nc has been removed by ncat

--- Obscurity/create/run.sh
+++ Obscurity/create/run.sh
@@ -6,6 +6,6 @@ reader_port=6543
 trap 'jobs -p | xargs kill -s 15' EXIT
 
 make tag reader || exit 1
-netcat -lp $tag_port --continuous -e ./tag &
-netcat -lp $reader_port --continuous -e ./reader &
+ncat -lp $tag_port -k -e ./tag &
+ncat -lp $reader_port -k -e ./reader &
 wait
