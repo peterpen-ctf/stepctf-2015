018c07b081d1f4380f68e1684ab0a2e96660b441
Anton Ovchinnikov <revolver112@gmail.com>
2015-04-29 17:46:24 +0200

    Catless: minor updates

--- Catless/create/bootstrap.sh
+++ Catless/create/bootstrap.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 rm -f \
-    /bin/less /usr/bin/tail /usr/bin/head /bin/*grep* \
+    /bin/less /usr/bin/tail* /usr/bin/head /bin/*grep* \
     /usr/bin/*grep* /etc/alternatives/v* /usr/bin/xxd \
     /usr/bin/python* /bin/cat /usr/bin/perl* /usr/bin/vi* /bin/more \
     /bin/sed /usr/bin/*diff* /usr/bin/curl /usr/bin/wget /usr/bin/base64 \
-    /bin/nc* /bin/netcat /usr/bin/nano /bin/nano
+    /bin/nc* /bin/netcat /usr/bin/nano /bin/nano /usr/bin/sort \
+    /usr/bin/awk /bin/echo
 
 # /usr/bin/cut?
 # /usr/bin/nl?
+
+# Greeting
+echo '### Welcome home! ###' > /.box/.greeting
+
+# Update bashrc
+echo 'alias echo=/bin/echo' >> /home/box/.bashrc
+
 
 echo "Noooo, you've managed to read me!
 
