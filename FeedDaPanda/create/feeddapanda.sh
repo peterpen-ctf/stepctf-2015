99c929a3482f197bbb2d495359057beb03013f80
Khanov Arthur <awengar@gmail.com>
2015-05-10 10:15:32 +0200

    feeddapanda's run-script has been changed

--- FeedDaPanda/create/feeddapanda.sh
+++ FeedDaPanda/create/feeddapanda.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo docker kill web100
-sudo docker rm web100
-sudo docker build -t patrick/web100 .
-sudo docker run -d --name web100 -p 127.0.0.1:13371:8080 patrick/web100
+sudo docker kill feeddapanda
+sudo docker rm feeddapanda
+sudo docker build -t patrick/feeddapanda .
+sudo docker run -d --name feeddapanda patrick/feeddapanda
